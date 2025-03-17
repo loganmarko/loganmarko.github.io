2178a307e13fecc7ad61545b93da0d6664d3de64
loganmarko <loganmarkowiak@ocadu.ca>
2025-03-17 17:38:29 -0400

    First Version

---
+++ script.js
@@ -0,0 +1,35 @@
+0;
+
+var kitty; //declared a variable to store space in memory;
+
+kitty = 5; //definition of the variable;
+
+var Kitty = 24;
+
+console.log(kitty);
+
+kitty = Kitty;
+
+console.log(kitty);
+
+var doggy;
+console.log(doggy);
+
+var prose = "this is a string";
+console.log(prose);
+
+//boolean
+var x = true;
+x = false; //booleans are true or false
+
+//objects
+var ball = {}; //empty object
+ball = null; //null is a data type that represents nothing
+
+//operators
+5 + 5; //addition statement
+kitty = 5 + 5;
+var kitty; //becomes undefined
+
+kitty = 5 + '5';
+console.log(kitty); //55
---
+++ sketch.js
@@ -0,0 +1,19 @@
+var x = 0;
+var y = 0;
+
+
+function setup(){
+    createCanvas(1300,480);
+}
+
+function draw(){
+    background('#40E0D0');
+
+    x = lerp(x, mouseX, 0.05);
+    y = lerp(y, mouseY, 0.05);
+
+    fill('#880808');
+    ellipse(x,y,50,50);
+    
+   
+}
